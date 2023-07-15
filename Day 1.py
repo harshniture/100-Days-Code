17e576ca5ad60265a0e087e1f238f74a9ae387c0
harshniture <harshniture12@gmail.com>
2023-07-15 16:15:26 +0530

    day1

--- Day 1.py
+++ Day 1.py
@@ -1,8 +1,6 @@
 #Take two inputs from the user. One will be an integer. The other will be a float number. Then multiply them to display the output.
 
-int_text = input()
-int_num = int(int_text)
-float_text = input()
-float_num = float(float_text)
+int_num = int(input())
+float_num = float(input())
 result = int_num * float_num
 print(result)
