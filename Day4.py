daa00af51b1f59f3266d8fa3bcb42d230559964f
harshniture <harshniture12@gmail.com>
2023-07-18 16:27:45 +0530

    day4

---
+++ Day4.py
@@ -0,0 +1,6 @@
+#Take two numbers from the users. Calculate the result of second number power of the first number.
+
+base_num = int(input('Give me the base number: '))
+power_num = int(input('Give me the power number: '))
+result = base_num ** power_num
+print('Your result is: ', result)
