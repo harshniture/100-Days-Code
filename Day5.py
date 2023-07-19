6fb4e4e1a824dcf3f3cab4d8921f7ca466366245
harshniture <harshniture12@gmail.com>
2023-07-19 16:30:50 +0530

    day5

---
+++ Day5.py
@@ -0,0 +1,11 @@
+#Swap two variables.
+#To swap two variables: the value of the first variable will come the value of the second variable. 
+# #On the other hand, the value of the second variable will become the value of the first variable.
+
+a = 5
+b = 7
+print('a, b', a, b)
+temp = a
+a = b
+b = temp
+print('a, b', a, b)
