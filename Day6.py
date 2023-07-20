0f2f806f37f7e4c935190e210a8baf5da5d9a748
harshniture <harshniture12@gmail.com>
2023-07-20 16:34:33 +0530

    day6

---
+++ Day6.py
@@ -0,0 +1,7 @@
+#Take the temperature in degrees Celsius and convert it to Fahrenheit.
+
+celsius = float(input("Enter temperature in degrees Celsius: "))
+ 
+fahrenheit = celsius*9/5+32
+ 
+print("Temperature in Fahrenheit:", fahrenheit)
