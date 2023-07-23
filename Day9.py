9a018cce1312ad3664d86faad68ed3ac0591173e
harshniture <harshniture12@gmail.com>
2023-07-23 16:48:20 +0530

    day9

---
+++ Day9.py
@@ -0,0 +1,19 @@
+#Convert a decimal number to binary number.
+
+def dec_to_binary(n):
+	bits = []
+	
+	while n > 0:
+		bits.append(n%2)
+		n = n // 2
+	bits.reverse()
+	
+	binary = ''
+	for bit in bits:
+		binary += str(bit)
+	return binary
+
+
+num = int(input("Your decimal number: "))
+binary = dec_to_binary(num)
+print("Your binary is:", binary)
