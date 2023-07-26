f02d8b7d5b56c936c6f312e38dd04d01c0a17870
harshniture <harshniture12@gmail.com>
2023-07-26 17:12:56 +0530

    day12

---
+++ Day12.py
@@ -0,0 +1,13 @@
+#Take money borrowed, interest and duration as input. Then, compute the compound interest rate.
+
+def compound_interest(principle, rate, time):
+	interest = principle * ((1 + rate / 100) ** time)
+	return interest
+
+principle = int(input("Money you borrowed: "))
+interest_rate = float(input("Interest Rate: "))
+time = float(input("Overall Duration: "))
+
+total_due = compound_interest(principle, interest_rate, time)
+
+print("Interest Amount is:", total_due)
