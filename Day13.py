52cef5e21abaf32a16f91e9e38437620837deafa
harshniture <harshniture12@gmail.com>
2023-07-27 17:22:07 +0530

    day13

--- Day13.py
+++ Day13.py
@@ -1,10 +1,10 @@
 #You borrowed $5000 for 2 years with 2% interest per year. Calculate the simple interest to know how much you have to pay?
 
-principle = int(input("Money you borrowed: "))
-interest_rate = float(input("Interest Rate: "))
-time = float(input("Overall Duration: "))
+principle = int(input("P: "))
+interest_rate = float(input("R: "))
+time = float(input("N: "))
 
 # Calculates simple interest
 simple_interest = principle * (interest_rate/100) * time
 
-print("Simple interest is:", simple_interest)
+print("Simple Interest:", simple_interest)
