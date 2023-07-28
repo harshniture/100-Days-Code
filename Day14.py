15996a5e94ad3cd6d544ddd504f949946dcc7693
harshniture <harshniture12@gmail.com>
2023-07-28 17:27:53 +0530

    day14

--- Day14.py
+++ Day14.py
@@ -1,9 +1,9 @@
 #Compute gravitational force between two objects.
 
-mass1 = float(input("First mass: "))
-mass2 = float(input("Second mass: "))
+mass1 = float(input("m1: "))
+mass2 = float(input("m2: "))
  
-r = float(input("Distance between the objects: "))
+r = float(input("distance: "))
  
 G = 6.673*(10**-11)
 force =(G*mass1*mass2)/(r**2)
