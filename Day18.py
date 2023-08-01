dcdb0cc47b3901939fc0bc0e096b85fc8ed15707
harshniture <harshniture12@gmail.com>
2023-08-01 17:36:20 +0530

    day18

---
+++ Day18.py
@@ -0,0 +1,13 @@
+#Find the smallest prime factor for the given number.
+
+def get_smallest_factor(num):
+   factor = 2
+   while num % factor != 0:
+       factor += 1
+   return factor
+ 
+num = int(input('Enter your number: '))
+ 
+smallest_factor = get_smallest_factor(num)
+ 
+print('The smallest prime factor is: ', smallest_factor)
