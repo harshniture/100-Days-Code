1d01aa4a4a4176265e827d7211c7f972f5cba9ea
harshniture <harshniture12@gmail.com>
2023-08-02 17:59:54 +0530

    day19

---
+++ Day19.py
@@ -0,0 +1,13 @@
+#Reverse a number.
+
+def reverse_num(num):
+   reverse = 0
+   while(num>0):
+       last_digit = num%10
+       reverse = reverse*10 + last_digit
+       num = num//10
+   return reverse
+ 
+n=int(input("Enter number: "))
+reverse = reverse_num(n)
+print("Reverse of the number:",reverse)
